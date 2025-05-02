04d8d73b202b96119392257c678949a24a7367ec
Juhyeon Lee <juhyeon@ix-dev.cs.uoregon.edu>
2025-05-02 14:50:57 -0700

    lab05 folder upload

---
+++ labs/lab05/src/ohno.c
@@ -0,0 +1,81 @@
+/*********************************************
+
+CIS330: Lab 3
+
+Implementation file for the error reporting system
+
+*********************************************/
+
+#include <ohno.h>
+#include <stdlib.h>
+#include <string.h>
+
+static struct ohno_state *state;
+
+/*
+ * Initialize the ohno error system with the given file stream and application name.
+ *
+ * This should allocate and populate the state structure.
+ * Make sure to make a copy of `app_name` as we'll need to use this string later when `ohno()` is called.
+ *
+ * Feel free to return non-zero if anything went wrong (like not having sufficient memory).
+ */
+int
+ohno_init(FILE* where_to, const char* app_name)
+{
+    state = (struct ohno_state*) malloc(sizeof(struct ohno_state));
+    if (state == NULL) return 1;
+
+    state->out = where_to;
+
+    int str_len = strlen(app_name);
+    state->name = (char*) malloc(sizeof(char) * (str_len + 1));
+    if (state->name == NULL) return 1;
+    for (int i = 0; i < str_len; i++) {
+        state->name[i] = app_name[i];
+    }
+    state->name[str_len] = '\0';
+
+    state->error_number = 0;
+
+
+  return 0;
+}
+
+/*
+ * Free any memory allocated to the ohno error system.
+ *
+ * You allocated memory in `ohno_init()`, now you must give it back.
+ */
+void
+ohno_free()
+{
+    free(state->name);
+    free(state);
+}
+
+/*
+ * Report an error or warning given the current ohno error system settings (from ohno_init())
+ *
+ * This function should format `message` and `severity` along with the `app_name` string copied in `ohno_init()`
+ * and write (print) a nice message on the saved `FILE *`.
+ * The particular formating is up to you. Get creative if your like and feel free to add useful information
+ * (e.g. error number or timestamp) to your report.
+ */
+void
+ohno(const char* message, ohno_severity_t severity)
+{
+    switch (severity) {
+        case OHNO_WARNING:
+            fprintf(state->out, "WARNING #%d in %s %s", state->error_number, state->name, message);
+            break;
+        case OHNO_SERIOUS:
+            fprintf(state->out, "SERIOUS #%d in %s %s", state->error_number, state->name, message);
+            break;
+        case OHNO_FATAL:
+            fprintf(state->out, "FATAL #%d in %s %s", state->error_number, state->name, message);
+            break;
+
+    }
+    state->error_number++;
+}
